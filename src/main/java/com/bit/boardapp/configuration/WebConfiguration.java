903cd71de4dcdc9b93f20480d99e0302fa3eae72
SonWuSeung <captain9802@gmail.com>
2024-03-12 15:26:25 +0900

    first commit

---
+++ src/main/java/com/bit/boardapp/configuration/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.bit.boardapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // 리액트 라우트 요청시 index.html로 가도록 설정
+        registry.addViewController("/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/**/{spring:\\w+}")
+                .setViewName("forward:/");
+        registry.addViewController("/{spring:\\w+}/**{spring:?~(\\.js|\\.css)$}")
+                .setViewName("forward:/");
+    }
+}
